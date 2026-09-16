c21c43958f31a5a5785781a9ec2ee6c5926b4883
pedroSaucedo02 <hpedromello122@gmail.com>
2026-09-16 15:52:53 -0300

    criação do TaskService

--- src/main/java/tarefa/services/TaskService.java
+++ src/main/java/tarefa/services/TaskService.java
@@ -1,27 +1,78 @@
+//pacote onde está a classe de serviço no projeto
 package tarefa.services;
 
+//importa List da biblioteca padrao do java para manipular colecoes de objetos
 import java.util.List;
+//importa optional, usado para tratar valores que podem nao estar presentes (evite nullexeceptionpointer)
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowire;
+//importa a anotacao do spring para a injeção automatica de dependencias
 import org.springframework.beans.factory.annotation.Autowired;
+//importa a anotacao que define essa classe como um componente de servico gerenciado pelo spring
 import org.springframework.stereotype.Service;
+//importa a anotacao para gerenciar transacoes no banco de dados (garante atomicidade na operacao)
 import org.springframework.transaction.annotation.Transactional;
 
+//importa o models.task
 import tarefa.java.models.Task;
+//importa o models.User
 import tarefa.java.models.User;
+//importa a interface do repositorio responsavel pelas operacoes no banco de dados
 import tarefa.java.repositories.TaskRepository;
 
+//anotacao que indica para o spring que essa classe contem as regras de negocios
 @Service 
-
-
 public class TaskService  {
-    
+    //injeta automaticamente a instancia do taskRepository gerenciado pelo spring
     @Autowired 
     private TaskRepository taskRepository;
+
+    //injeta autoaticamente a instancia do Userservice para validar o usuario
     @Autowired 
     private UserService userservice;
-    
 
+    //metodo para buscar task apartir do ID
+    public Task findById(Long Id){
+        //executa a busca do banco, retorna um optional contendo (ou nao) a task
+        Optional<Task> task = this.taskRepository.findById(Id);
+    
+        //se a tarefa existir, retorna o objeto, se estiver vazia, lança i, RunTimeException
+        return task.orElseThrow(()-> new RuntimeException(
+            "Tarefa não encontrada! Id:" + Id + ",Tipo:" + Task.class.getName()
+        ));
+    }
+    //metodo para buscar todas as tarefas vinculadar a um determinado usuario
+    public List<Task> finByUserId(Long UserId){
+
+        //chama o UserService para garantir que o usuario existe no banco(lanca execoes se nao existir)
+        this.UserService.findById(UserId);
+    
+        //Executa a busca customizada no repositorio filtrando pelo id do usuario
+        List<Task> task = this.taskRepository.findByUser_Id(UserId);
+
+        //retorna a lista de tarefas
+        return task;
+    
+        //garante que a criacao ocorra dentro de uma transação de banco de dados (rolback automatico se falhas)
+        @Transactional 
+        public Task create(Task obj){
+            //valida se o usuario informado no objeto realmente existe no banco e recupera seus dados
+            User user = this.userservice.findById(obj.getUser().getId());
+        
+            //define o ID como null para garantir que o JPA realize um inserção(INSERT) e não uma atualização
+            obj.setId(id:null);
+        
+            //associa a entida User completa e validada a tarefa
+            obk.setUser(user);
+
+            //salva a nova tarefa no banco de dados e atualiza 'obj' com o ID gerado
+            obj = this.taskRepository.save(obj);
+
+            return ibj;
+        }
+        
+        @Transactional 
+        public Task update(Task obj)
+    }
 
 }
